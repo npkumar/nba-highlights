f62a8329fa8045a57d53e44ee3c194cb93a3bdca
Nitin Pradeep Kumar <getnpk@gmail.com>
2018-02-13 01:23:04 +0800

    Add styling for Slider Featured component

---
+++ src/components/featured.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Slider from 'react-slick';
+
+const settings = {
+  arrows: false,
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1
+};
+
+const generateSlides = ({ slides }) => {
+  if (slides) {
+    return (
+      <Slider {...settings}>
+        {
+          slides.map(slide => {
+            return (
+              <div
+                key={slide.id}
+                className="item-slider"
+                style={{
+                  background: `url(images/covers/${slide.cover}) no-repeat`,
+                }}
+              >
+                <div className="caption">
+                  <h4>{ slide.topic }</h4>
+                  <p>{ slide.title }</p>
+                </div>
+              </div>
+            )
+          })
+        }
+      </Slider>
+    )
+  }
+}
+
+const Featured = (props) => {
+    return (
+      <div>
+        {generateSlides(props)}
+      </div>
+    )
+}
+
+export default Featured;
--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 
+import Featured from './featured';
+
+const URL = 'http://localhost:3004/home';
+
 class Home extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      home: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch(URL, {
+      method: 'GET'
+    })
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        home: json
+      });
+    })
+  }
+
   render() {
     return (
       <div>
-        Home
+        <Featured slides={this.state.home.slider}/>
       </div>
     );
   }
