b9acc409304481c2e002483679c2b5fab80640d0
Nitin Pradeep Kumar <getnpk@gmail.com>
2018-02-16 19:25:06 +0800

    Add team detail page

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Header from './header';
 import Footer from './footer';
 import Home from './home';
 import Teams from './teams';
+import Team from './team';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
           <Header/>
           <Route exact path="/" component={Home}></Route>
           <Route exact path="/teams" component={Teams}></Route>
+          <Route exact path="/team/:id" component={Team}></Route>
           <Footer/>
         </div>
       </BrowserRouter>
